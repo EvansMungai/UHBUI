8530ddc4791059e13215257ebfafa105459e66fb
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2024-10-22 10:54:14 +0300

    Create Footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from "../../components/elements/hero/hero.component";
 import { NavbarComponent } from "../../components/elements/navbar/navbar.component";
+import { FooterComponent } from "../../components/elements/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroComponent, NavbarComponent],
+  imports: [RouterOutlet, HeroComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
