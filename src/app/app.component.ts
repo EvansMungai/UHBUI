d542487c775bc0470d895c44c3c42b1e09acee52
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2024-10-22 10:46:25 +0300

    Create hero component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from "../../components/elements/hero/hero.component";
 import { NavbarComponent } from "../../components/elements/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, HeroComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
