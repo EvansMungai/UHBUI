abf22ed22f3cb495081526cf9d4e032a2fe5d7b0
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2025-02-12 22:13:25 +0300

    Add routing to the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { DrawerComponent } from "../../components/elements/drawer/drawer.component";
-
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [DrawerComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
