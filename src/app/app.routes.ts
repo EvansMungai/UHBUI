4b334957fd67e591d7a31eacc43521a01f190708
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2025-02-13 07:59:55 +0300

    Add admin page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LandingComponent } from './landing/landing.component';
 import { StudentComponent } from './student/student.component';
 import { HousekeeperComponent } from './housekeeper/housekeeper.component';
 import { MatronComponent } from './matron/matron.component';
+import { AdminComponent } from './admin/admin.component';
 
 export const routes: Routes = [
     { path: '', component: LandingComponent },
     { path: 'student', component: StudentComponent },
     { path: 'housekeeper', component: HousekeeperComponent },
-    { path: 'matron', component: MatronComponent }
+    { path: 'matron', component: MatronComponent },
+    { path: 'admin', component: AdminComponent}
 ];
