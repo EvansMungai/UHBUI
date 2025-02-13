d42d00a83fe73aadfe606f780e3757e2bcf45934
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2025-02-13 07:57:23 +0300

    Add matron page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 import { StudentComponent } from './student/student.component';
 import { HousekeeperComponent } from './housekeeper/housekeeper.component';
+import { MatronComponent } from './matron/matron.component';
 
 export const routes: Routes = [
-    { path: '', component: LandingComponent},
-    { path: 'student', component: StudentComponent},
-    {path: 'housekeeper', component: HousekeeperComponent}
+    { path: '', component: LandingComponent },
+    { path: 'student', component: StudentComponent },
+    { path: 'housekeeper', component: HousekeeperComponent },
+    { path: 'matron', component: MatronComponent }
 ];
---
+++ src/app/matron/matron.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { DrawerComponent } from "../../../components/elements/drawer/drawer.component";
+
+@Component({
+  selector: 'app-matron',
+  standalone: true,
+  imports: [DrawerComponent],
+  templateUrl: './matron.component.html',
+  styleUrl: './matron.component.css'
+})
+export class MatronComponent {
+
+}
