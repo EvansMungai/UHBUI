c426f04b93e6d4cc36d8a9211291efd073146cc4
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2025-02-20 17:15:06 +0300

    Create hostel application form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { StudentComponent } from './student/student.component';
 import { HousekeeperComponent } from './housekeeper/housekeeper.component';
 import { MatronComponent } from './matron/matron.component';
 import { AdminComponent } from './admin/admin.component';
+import { BookingComponent } from './student/booking/booking.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 
 export const routes: Routes = [
@@ -12,5 +13,6 @@ export const routes: Routes = [
     { path: 'housekeeper', component: HousekeeperComponent },
     { path: 'matron', component: MatronComponent },
     { path: 'admin', component: AdminComponent},
+    { path: 'booking', component: BookingComponent},
     { path: '**', component:NotfoundComponent}
 ];
