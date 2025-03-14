2649545ffb267cb223b333b90c56d426258f1a2f
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2025-03-14 11:33:45 +0300

    Create Housekeeper dashboard page

--- components/elements/table/table.component.ts
+++ components/elements/table/table.component.ts
@@ -12,6 +12,7 @@ import { StudentData } from '../../interfaces/studentData';
 export class TableComponent implements OnInit {
   @Input() TableData: any[] = [];
   tableHeaders: string[] = [];
+  @Input() showActionColumn: boolean = false;
   constructor() { }
   ngOnInit(): void {
       this.tableHeaders = Object.keys(this.TableData[0]);
