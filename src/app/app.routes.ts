b900864689620bc0b3df3535cf395cb49a4b5fb5
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2025-03-15 12:52:02 +0300

    Update styling in housekeeper and matron dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -42,7 +42,6 @@ export const routes: Routes = [
                 ]
             },
             { path: 'admin', component: AdminComponent },
-            { path: 'booking', component: BookingComponent }
         ]
     },
     { path: '**', component: NotfoundComponent }
