4a4b6e4e9801d2176553e6d5bfb6281aa1e9a2a6
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2025-03-19 19:06:33 +0300

    Make the registration form a reactive form and add necessary validators.

--- src/app/student/register-student-details/register-student-details.component.ts
+++ src/app/student/register-student-details/register-student-details.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ButtonComponent } from "../../../../components/elements/button/button.component";
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SubmitButton } from '../../../../components/interfaces/button.interface';
 
 @Component({
   selector: 'app-register-student-details',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [CommonModule, ReactiveFormsModule, ButtonComponent],
   templateUrl: './register-student-details.component.html',
   styleUrl: './register-student-details.component.css'
 })
 export class RegisterStudentDetailsComponent {
+  studentProfileForm: FormGroup;
+  submitButtonProps: SubmitButton;
 
+  constructor(private fb: FormBuilder) {
+    this.studentProfileForm = this.fb.group({
+      registrationNo: ['', [Validators.required, Validators.pattern('^[A-Z]\\d{3}-\\d{2}-\\d{4}/\\d{4}$')]],
+      surname: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      gender: ['', Validators.required]
+    });
+    this.submitButtonProps = {
+      text: 'Submit', type: 'submit', variant: "secondary", formId: 'studentRegistrationForm'
+    }
+  }
+  onSubmit(): void {
+    if (this.studentProfileForm.valid) {
+      console.log(this.studentProfileForm.value);
+    } else {
+      console.log('Form is invalid')
+    }
+  }
 }
