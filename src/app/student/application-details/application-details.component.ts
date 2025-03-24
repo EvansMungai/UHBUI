a9b543c3ab80011568ef563dc946ae1b9264b586
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2025-03-24 19:23:54 +0300

    Consume student details from api.

--- src/app/student/application-details/application-details.component.ts
+++ src/app/student/application-details/application-details.component.ts
@@ -1,25 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { TableComponent } from "../../../../components/elements/table/table.component";
 import { StudentService } from '../../../../components/services/student.service';
 import { CardComponent } from "../../../../components/elements/card/card.component";
 import { TableColumn } from '../../../../components/interfaces/table.interface';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-application-details',
   standalone: true,
-  imports: [TableComponent, CardComponent],
+  imports: [CommonModule, TableComponent, CardComponent],
   templateUrl: './application-details.component.html',
   styleUrl: './application-details.component.css'
 })
 export class ApplicationDetailsComponent {
   cardTitle: string = "Application Details";
-  tableData: any[] = [];
+  tableData: Observable<any> = {} as Observable<any>;
+  regNo: string = 'C026-01-0920/2022';
   tableColumns: TableColumn[] = [
-    { key: 'ApplicationPeriod', header: "Application Period", sortable: false },
-    { key: 'RegistrationNo', header: "Registration Number", sortable: false },
-    { key: 'Status', header: "Status", sortable: false }
+    { key: 'applicationPeriod', header: "Application Period", sortable: false },
+    { key: 'registrationNo', header: "Registration Number", sortable: false },
+    { key: 'status', header: "Status", sortable: false }
   ];
   constructor(private studentService: StudentService) {
-    this.tableData = this.studentService.getApplicationDetails();
+    this.tableData = this.studentService.getApplicationData(this.regNo);
   }
 }
--- src/app/student/dashboard/dashboard.component.ts
+++ src/app/student/dashboard/dashboard.component.ts
@@ -1,25 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { CardComponent } from '../../../../components/elements/card/card.component';
 import { StudentService } from '../../../../components/services/student.service';
 import { TableComponent } from "../../../../components/elements/table/table.component";
 import { TableColumn } from '../../../../components/interfaces/table.interface';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'student-dashboard',
   standalone: true,
-  imports: [CardComponent, TableComponent],
+  imports: [CommonModule, CardComponent, TableComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class StudentDashboardComponent {
   cardTitle: string = "Student Details"
-  tableData: any[] = [];
+  tableData: Observable<any> = {} as Observable<any>;
   tableColumns: TableColumn[] = [
-    { key: 'RegistrationNo', header: 'Registration Number'},
-    { key: 'Surname', header: 'Surname' },
-    { key: 'FirstName', header: 'First Name' },
-    { key: 'SecondName', header: 'Second Name' },
-    { key: 'Gender', header: 'Gender' },
+    { key: 'regNo', header: 'Registration Number' },
+    { key: 'surname', header: 'Surname' },
+    { key: 'firstName', header: 'First Name' },
+    { key: 'secondName', header: 'Second Name' },
+    { key: 'gender', header: 'Gender' },
   ];
   constructor(private studentService: StudentService) {
     this.tableData = this.studentService.getStudentData();
