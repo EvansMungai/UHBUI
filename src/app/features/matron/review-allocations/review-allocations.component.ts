368e68549bdddd1205bc242f46fbcbe89ce3c20d
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2025-08-30 01:18:05 +0300

    feat(applications): Show successful applications to matron dashboard.

--- src/app/features/matron/review-allocations/review-allocations.component.ts
+++ src/app/features/matron/review-allocations/review-allocations.component.ts
@@ -1,23 +1,27 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { CommonModule } from '@angular/common';
 import { CardComponent } from '../../../shared/elements/card/card.component';
 import { TableComponent } from '../../../shared/elements/table/table.component';
+import { LoadingComponent } from '../../../shared/elements/loading/loading.component';
 import { TableColumn, TableAction } from '../../../core/interfaces/table.interface';
-import { Router } from '@angular/router';
 import { ApplicationService } from '../../../core/services/application.service';
-import { Observable } from 'rxjs';
+import { RoomService } from '../../../core/services/room.service';
 
 @Component({
   selector: 'app-review-allocations',
-  imports: [CommonModule, CardComponent, TableComponent],
+  imports: [CommonModule, CardComponent, TableComponent, LoadingComponent],
   templateUrl: './review-allocations.component.html',
   styleUrl: './review-allocations.component.css'
 })
-export class ReviewAllocationsComponent {
+export class ReviewAllocationsComponent implements OnInit {
   private applicationService = inject(ApplicationService);
+  private roomService = inject(RoomService);
   private router = inject(Router);
 
-  tableData: Observable<any> = {} as Observable<any>;
+  readonly tableData = signal<any | null>(null);
+  readonly roomData = signal<any | null>(null);
   tableColumns: TableColumn[] = [
     { key: 'applicationPeriod', header: 'Application Period' },
     { key: 'registrationNo', header: 'Registration Number' },
@@ -25,11 +29,17 @@ export class ReviewAllocationsComponent {
     { key: 'preferredHostel', header: 'Preferred Hostel' }
   ];
   tableActions: TableAction[] = [{ buttonProps: { text: 'Allocate Room', type: 'button', variant: 'secondary', size: 'sm', action: (row: any, index: number) => this.navigateToAllocationRoute(row, index) } }]
+  loadingStyles: string = 'loading-spinner loading-lg';
 
-  constructor() {
-    const applicationService = this.applicationService;
-
-    this.tableData = applicationService.getAcceptedApplications();
+  ngOnInit(): void {
+    this.applicationService.getAcceptedApplications().subscribe({
+      next: data => this.tableData.set(data),
+      error: err => console.error('Error fetching accepted applications: ', err)
+    });
+    this.roomService.getRoomsData().subscribe({
+      next: data => this.roomData.set(data),
+      error: err => console.error('Error fetching rooms data')
+    });
   }
 
   navigateToAllocationRoute(row: any, index: number) {
