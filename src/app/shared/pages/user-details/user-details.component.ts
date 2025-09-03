bafdc91af3d95dcae8be7cf8e874ceaca906616d
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2025-09-03 12:19:20 +0300

    feat(user): Return relevant user details to logged in users.

--- src/app/shared/pages/user-details/user-details.component.ts
+++ src/app/shared/pages/user-details/user-details.component.ts
@@ -3,10 +3,12 @@ import { Component, OnInit, signal } from '@angular/core';
 
 import { ChangePasswordComponent } from "../change-password/change-password.component";
 import { LoadingComponent } from '../../elements/loading/loading.component';
+import { ButtonComponent } from '../../elements/button/button.component';
+import { ActionButton } from '../../../core/interfaces/button.interface';
 
 @Component({
   selector: 'app-user-details',
-  imports: [ChangePasswordComponent, LoadingComponent],
+  imports: [ChangePasswordComponent, LoadingComponent, ButtonComponent],
   templateUrl: './user-details.component.html',
   styleUrl: './user-details.component.css'
 })
@@ -15,6 +17,8 @@ export class UserDetailsComponent implements OnInit {
   showChangePasswordForm: boolean = false;
   userData = signal<any | null>(null);
   loadingStyles = 'loading-spinner loading-lg';
+  buttonProps: ActionButton = { text: 'Change Password', type: 'button', variant: 'secondary', size: 'sm', action: () => this.toggleChangePasswordForm() }
+
 
   ngOnInit(): void {
     const userDetails = localStorage.getItem('user');
