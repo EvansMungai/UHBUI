878faa1745ef988a8dc205d71733b81a562b7444
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2025-09-03 12:15:27 +0300

    feat(user): Return relevant user details to logged in users.

--- src/app/shared/pages/user-details/user-details.component.ts
+++ src/app/shared/pages/user-details/user-details.component.ts
@@ -1,15 +1,30 @@
 
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+
 import { ChangePasswordComponent } from "../change-password/change-password.component";
+import { LoadingComponent } from '../../elements/loading/loading.component';
 
 @Component({
-    selector: 'app-user-details',
-    imports: [ChangePasswordComponent],
-    templateUrl: './user-details.component.html',
-    styleUrl: './user-details.component.css'
+  selector: 'app-user-details',
+  imports: [ChangePasswordComponent, LoadingComponent],
+  templateUrl: './user-details.component.html',
+  styleUrl: './user-details.component.css'
 })
-export class UserDetailsComponent {
+
+export class UserDetailsComponent implements OnInit {
   showChangePasswordForm: boolean = false;
+  userData = signal<any | null>(null);
+  loadingStyles = 'loading-spinner loading-lg';
+
+  ngOnInit(): void {
+    const userDetails = localStorage.getItem('user');
+    if (userDetails) {
+      const parsedUser = JSON.parse(userDetails);
+      this.userData.set(parsedUser);
+    } else {
+      console.warn('No user data found in the local storage');
+    }
+  }
   toggleChangePasswordForm(): boolean {
     return this.showChangePasswordForm = !this.showChangePasswordForm;
   }
