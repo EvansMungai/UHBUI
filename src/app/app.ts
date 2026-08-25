0c899e590dd5b8fb28cd82f14ab774bb67b7e2ed
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2026-08-25 12:24:29 +0300

    feat(toast): implement toast component.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,11 @@
-import { Component, inject, signal } from '@angular/core';
-import { Toast } from './shared/components/toast/toast';
-import { ToastService } from './core/services/toast';
+import { Component, signal } from '@angular/core';
 
 @Component({
-  imports: [Toast],
+  imports: [],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html'
 })
 export class App {
   protected readonly title = signal('uhb');
-  private toastService = inject(ToastService);
-
-  showSuccessToast(){
-    this.toastService.add({
-      key: 'main-toast',
-      severity: 'success',
-      summary: 'Success Alert!',
-      detail: 'Your custom configuration has saved cleanly',
-      life: 2000
-    })
-  }
 }
