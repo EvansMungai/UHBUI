c92a17b58b5c2c8f38814033f62bdc63cfbea668
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2026-08-26 23:53:41 +0300

    feat(student): create booking page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,18 @@ import { Routes } from '@angular/router';
 import { Landing } from './shared/pages/landing/landing';
 import { Auth } from './shared/pages/auth/auth';
 import { WebPart } from './shared/layouts/web-part/web-part';
-import { StudentDashboard } from './shared/features/student/student-dashboard/student-dashboard';
 
 export const routes: Routes = [
     { path: '', component: Landing },
     { path: 'auth', component: Auth },
     {
-        path: 'uhb', component: WebPart, children: [
-            { path: 'student', component: StudentDashboard }
+        path: 'uhb', children: [
+            {
+                path: 'student', component: WebPart, children: [
+                    { path: '', loadComponent: () => import('./shared/features/student/student-dashboard/student-dashboard').then(m => m.StudentDashboard) },
+                    { path: 'booking', loadComponent: () => import('./shared/features/student/booking/booking').then(m => m.Booking) }
+                ]
+            }
         ]
     },
 ];
