bafd8f53baea1c7e947a3b0c401fae1691fa5f88
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2026-08-26 15:31:05 +0300

    feat(auth): implement authentication pages.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Landing } from './shared/pages/landing/landing';
+import { Auth } from './shared/pages/auth/auth';
 
 export const routes: Routes = [
-    {path: '', component: Landing}
+    { path: '', component: Landing },
+    { path: 'auth', component: Auth }
 ];
---
+++ src/app/shared/pages/auth/auth.ts
@@ -0,0 +1,70 @@
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+type AuthTab = 'signin' | 'signup';
+
+@Component({
+  imports: [ReactiveFormsModule, RouterLink],
+  selector: 'app-auth',
+  styleUrl: './auth.css',
+  templateUrl: './auth.html',
+})
+export class Auth {
+  private readonly fb = inject(FormBuilder);
+
+  activeTab: AuthTab = 'signup';
+  loading = signal<boolean>(false);
+
+  readonly signInForm = this.fb.nonNullable.group({
+    email: ['', Validators.required, Validators.email],
+    password: ['', Validators.required],
+    rememberMe: false
+  });
+
+  readonly signUpForm = this.fb.nonNullable.group({
+    email: ['', Validators.required, Validators.email],
+    password: ['', Validators.required]
+  });
+
+  setTab(tab: AuthTab): void {
+    this.activeTab = tab;
+  };
+
+  signIn(): void {
+    if (this.signInForm.invalid) {
+      this.signInForm.markAllAsTouched();
+      return;
+    }
+
+    this.loading.set(true);
+
+    const credentials = this.signInForm.getRawValue();
+    console.log('Sign in:', credentials);
+
+    // this.authService.signIn(credentials).subscribe({
+    //   next: () => ...,
+    //   error: () => ...,
+    //   complete: () => this.loading = false,
+    // });
+  }
+
+  signUp(): void {
+    if (this.signUpForm.invalid) {
+      this.signUpForm.markAllAsTouched();
+      return;
+    }
+
+    this.loading.set(true);
+
+    const credentials = this.signUpForm.getRawValue();
+
+    console.log('Sign up:', credentials);
+
+    // this.authService.signUp(credentials).subscribe({
+    //   next: () => ...,
+    //   error: () => ...,
+    //   complete: () => this.loading = false,
+    // });
+  }
+}
