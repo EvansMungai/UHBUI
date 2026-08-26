ad9a1e9c434e062930a951d699fdae22cb999ab0
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2026-08-26 16:18:20 +0300

    styles(web-part): styles dropdown menu in web-part

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Landing } from './shared/pages/landing/landing';
 import { Auth } from './shared/pages/auth/auth';
+import { WebPart } from './shared/layouts/web-part/web-part';
 
 export const routes: Routes = [
     { path: '', component: Landing },
-    { path: 'auth', component: Auth }
+    { path: 'auth', component: Auth },
+    { path: 'uhb', component: WebPart },
 ];
