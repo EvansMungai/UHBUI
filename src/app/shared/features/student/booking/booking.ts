7db8d895e2b577ca371217b518ab9676096eb6be
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2026-08-28 01:29:57 +0300

    feat(booking): implement booking form component.

--- src/app/shared/features/student/booking/booking.ts
+++ src/app/shared/features/student/booking/booking.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
-import { Card } from "../../../components/card/card";
+import { Component, signal } from '@angular/core';
+import { ApplicationData } from '../../../../core/models/application';
+import { form, required, schema, FormRoot, FormField } from '@angular/forms/signals';
 
 @Component({
-  imports: [Card],
+  imports: [FormRoot, FormField],
   selector: 'app-booking',
   styleUrl: './booking.css',
   templateUrl: './booking.html',
 })
 export class Booking {
+  loading = signal<boolean>(false);
+  bookingModel = signal<ApplicationData>({ applicationPeriod: '', registrationNo: '', preferredHostel: '', status: '', roomNo: '', disability: false, disabilityDetails: 'N/A', accommodatedBefore: false, accommodationPeriod: '', isSponsored: false, sponsor: '', receivesHelb: false, helbAmount: '', receivedBursary: false, bursaryAmount: '', workStudyBenefitsBefore: false, workStudyPeriod: '', specialExamsOnFinancialGrounds: false, specialExamPeriod: '', reasonsForConsideration: '' });
+  bookingForm = form(this.bookingModel, schema(path => {
+    required(path.registrationNo, { message: "Registration number is required" });
+    required(path.applicationPeriod, { message: "Application period is required" });
+    // required(path.disability, { message: "Disibility value is required" });
+    // required(path.accommodatedBefore, { message: "Accommodated before value is required" });
+    // required(path.isSponsored, { message: "Is Sponsored value is required" });
+    // required(path.receivesHelb, { message: "Receives Helb value is required" });
+    // required(path.receivedBursary, { message: "Receives Bursary value is required" });
+    // required(path.workStudyBenefitsBefore, { message: "Work Study Benefits Before value is required" });
+    // required(path.specialExamsOnFinancialGrounds, { message: "Special Exam before value is required" });
+    required(path.reasonsForConsideration, { message: "Reasons for Consideration is required" });
+  }), {
+    submission: {
+      action: async (field) => {
+
+      }
+    }
+  })
 }
