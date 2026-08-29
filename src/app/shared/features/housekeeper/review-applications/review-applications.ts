001829c04b162507e6e3f9700d1ccae7320dc2da
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2026-08-29 22:35:05 +0300

    feat(housekeeper): fetch applications list.

--- src/app/shared/features/housekeeper/review-applications/review-applications.ts
+++ src/app/shared/features/housekeeper/review-applications/review-applications.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
 import { Card } from '../../../components/card/card';
+import { httpResource, HttpErrorResponse } from '@angular/common/http';
+import { ApplicationData } from '../../../../core/models/application';
+import { environment } from '../../../../../environments/environment';
+import { TableColumn } from '../../../../core/Table';
+import { Table } from '../../../components/table/table';
+import { COMPOSITION_BUFFER_MODE } from '@angular/forms';
 
 @Component({
-  imports: [Card],
+  imports: [Card, Table],
   selector: 'app-review-applications',
   styleUrl: './review-applications.css',
   templateUrl: './review-applications.html',
 })
 export class ReviewApplications {
+  applicationResource = httpResource<ApplicationData[]>(() => {
+    return {
+      url: `${environment.apiUrl}/applications`,
+      method: 'GET'
+    }
+  });
+  applicationData = this.applicationResource.value;
+
+
+  tableColumns: TableColumn<ApplicationData>[] = [
+    { key: 'applicationPeriod', label: 'Application Period' },
+    { key: 'registrationNo', label: 'Registration Number' },
+    { key: 'status', label: 'Application Status' },
+    { key: 'preferredHostel', label: 'Preferred Hostel' }
+  ];
+  isNotFoundError(error: Error | null): boolean {
+    return error instanceof HttpErrorResponse && error.status === 404;
+  }
 }
