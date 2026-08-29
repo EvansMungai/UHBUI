9022d5d09237fcd89a4cb13e1fa32d7c3dd41a5d
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2026-08-29 20:38:19 +0300

    feat(dashboard): finish creating admin dashboard.

--- src/app/shared/components/table/table.ts
+++ src/app/shared/components/table/table.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input, signal } from '@angular/core';
 import { TableColumn } from '../../../core/Table';
 
 @Component({
@@ -11,8 +11,61 @@ export class Table<T extends object> {
   readonly data = input<T[]>([]);
   readonly columns = input<TableColumn<T>[]>([]);
   readonly emptyMessage = input('No data found');
+  tableStyles = input<string>('w-full text-sm text-left rtl:text-right');
+  headingStyles = input<string>('bg-accent dark:bg-neutral');
+  bodyStyles = input<string>('border border-secondary hover:bg-blue-200 dark:hover:bg-accent');
 
-  tableStyles = input<string>('w-full text-sm text-left rtl:text-right text-body');
-  headingStyles = input<string>('text-sm text-body bg-neutral-secondary-soft border-b rounded-base border-default');
-  bodyStyles = input<string>('bg-neutral-primary border-t border-default');
+  pageSize = input<number>(5);
+  currentPage = signal<number>(1);
+  totalPages = computed(() => {
+    const totalRecords = this.data().length;
+    const size = this.pageSize();
+
+    return size > 0 ? Math.ceil(totalRecords / size) : 0;
+  });
+  paginatedData = computed(() => {
+    const start = (this.currentPage() - 1) * this.pageSize();
+    const end = start + this.pageSize();
+
+    return this.data().slice(start, end);
+  })
+  startRecord = computed(() => {
+    if (this.data().length === 0) {
+      return 0;
+    }
+
+    return (this.currentPage() - 1) * this.pageSize() + 1;
+  });
+
+  endRecord = computed(() => {
+    return Math.min(
+      this.currentPage() * this.pageSize(),
+      this.data().length
+    );
+  });
+
+  pages = computed(() => {
+    const total = this.totalPages();
+
+    return Array.from(
+      { length: total },
+      (_, index) => index + 1
+    );
+  });
+
+  goToPage(page: number): void {
+    this.currentPage.set(page);
+  }
+
+  previousPage(): void {
+    if (this.currentPage() > 1) {
+      this.currentPage.update(page => page - 1);
+    }
+  }
+
+  nextPage(): void {
+    if (this.currentPage() < this.totalPages()) {
+      this.currentPage.update(page => page + 1);
+    }
+  }
 }
--- src/app/shared/features/administrator/admin-dashboard/admin-dashboard.ts
+++ src/app/shared/features/administrator/admin-dashboard/admin-dashboard.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
 import { Card } from '../../../components/card/card';
+import { httpResource } from '@angular/common/http';
+import { HostelData, RoomData } from '../../../../core/models/hostel';
+import { environment } from '../../../../../environments/environment';
+import { Table } from "../../../components/table/table";
+import { TableColumn } from '../../../../core/Table';
 
 @Component({
-  imports: [Card],
+  imports: [Card, Table],
   selector: 'app-admin-dashboard',
   styleUrl: './admin-dashboard.css',
   templateUrl: './admin-dashboard.html',
 })
 export class AdminDashboard {
+  hostelResource = httpResource<HostelData[]>(() => {
+    return {
+      url: `${environment.apiUrl}/hostels`,
+      method: 'GET'
+    }
+  });
+  hostelData = this.hostelResource.value;
+
+  roomResource = httpResource<RoomData[]>(() => {
+    return {
+      url: `${environment.apiUrl}/rooms`,
+      method: 'GET'
+    }
+  });
+  roomData = this.roomResource.value;
+
+  roomColumns: TableColumn<RoomData>[] = [
+    { key: 'hostelNo', label: 'Hostel No' },
+    { key: 'roomNo', label: 'Room No' },
+  ]
+
+  hostelColumns: TableColumn<HostelData>[] = [
+    { key: 'hostelNo', label: 'Hostel No' },
+    { key: 'hostelName', label: 'Hostel Name' },
+    { key: 'capacity', label: 'Capacity' },
+    { key: 'type', label: 'Hostel Type' },
+  ]
 }
